7b1ab7a4efe5157c3340709a88d17901cc2ec732
boudtech <boudtech@gmail.com>
2023-06-11 09:38:56 -0400

    Update main.tf
    Final fix for aws_key_pair.id in EC2 Instance

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_instance" "ec2" {
   ami = var.ec2_ami
   instance_type = var.instance_type
   subnet_id = aws_subnet.private.id
-  key_name = aws_key_pair.ec2_key.public_key
+  key_name = aws_key_pair.ec2_key.id
 
   root_block_device {
     delete_on_termination = true
